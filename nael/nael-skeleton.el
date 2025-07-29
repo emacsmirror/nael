422b83d16fa5729a4211a800bb77c7986922093c
Mekeor Melire <mekeor@posteo.de>
2025-07-29 14:29:22 +0200

    [skeleton] add
    todo: define three abbrev tables

---
+++ nael/nael-skeleton.el
@@ -0,0 +1,44 @@
+;;; nael-skeleton.el --- Skeleton for Nael  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2025 Mekeor Melire
+
+;; This file is NOT part of GNU Emacs.
+
+;; This code is licensed under GNU General Public License version 3,
+;; see LICENSE.GPL3.
+
+;;; Commentary:
+
+;; TODO
+
+;;; Code:
+
+(require 'skeleton)
+
+(define-skeleton nael-skeleton-7b7d nil nil "{" _ "}")
+(define-skeleton nael-skeleton-7b7d5f nil nil "{" _ "}_")
+(define-skeleton nael-skeleton-29832984 nil nil "⦃" _ "⦄")
+(define-skeleton nael-skeleton-5b5d nil nil "[" _ "]")
+(define-skeleton nael-skeleton-5b5d5f nil nil "[" _ "]_")
+(define-skeleton nael-skeleton-27e627e7 nil nil "⟦" _ "⟧")
+(define-skeleton nael-skeleton-27e827e9 nil nil "⟨" _ "⟩")
+(define-skeleton nael-skeleton-2829 nil nil "(" _ ")")
+(define-skeleton nael-skeleton-28295f nil nil "(" _ ")_")
+(define-skeleton nael-skeleton-27ee27ef nil nil "⟮" _ "⟯")
+(define-skeleton nael-skeleton-2e282e29 nil nil "⸨" _ "⸩")
+(define-skeleton nael-skeleton-2039203a nil nil "‹" _ "›")
+(define-skeleton nael-skeleton-abbb nil nil "«" _ "»")
+(define-skeleton nael-skeleton-20452046 nil nil "⁅" _ "⁆")
+(define-skeleton nael-skeleton-20162016 nil nil "‖" _ "‖")
+(define-skeleton nael-skeleton-20162016208a nil nil "‖" _ "‖₊")
+(define-skeleton nael-skeleton-20162016 nil nil "‖" _ "‖")
+(define-skeleton nael-skeleton-230a230b nil nil "⌊" _ "⌋")
+(define-skeleton nael-skeleton-23082309 nil nil "⌈" _ "⌉")
+(define-skeleton nael-skeleton-230a230b208a nil nil "⌊" _ "⌋₊")
+(define-skeleton nael-skeleton-23082309208a nil nil "⌈" _ "⌉₊")
+(define-skeleton nael-skeleton-298b298c nil nil "⦋" _ "⦌")
+(define-skeleton nael-skeleton-298b298c nil nil "⦋" _ "⦌")
+
+(provide 'nael-skeleton)
+
+;;; nael-skeleton.el ends here
