1e01178f751c9e34927f322a1f8a6d909b15405d
Mekeor Melire <mekeor@posteo.de>
2025-11-30 02:13:32 +0100

    [dir-locals] remove jinx-dir-local-words
    When jinx is not installed, Emacs doesn't know this variable is safe.
    It then asks the user for confirmation.

--- .dir-locals.el
+++ .dir-locals.el
@@ -5,7 +5,5 @@
    . "https://codeberg.org/mekeor/nael/issues/%s")
   (compile-command
    . "./build.el")
-  (jinx-dir-local-words
-   . "ElDoc Nael Mekeor Melire reindent")
   (sentence-end-double-space
    . t)))
